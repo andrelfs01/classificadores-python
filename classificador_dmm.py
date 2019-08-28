5fcfa4a669f21e5ad2585af4a005f263c6076640
andrelfs01 <andre.luisfs01@gmail.com>
2019-08-28 00:13:45 -0300

    att

--- classificador_dmm.py
+++ classificador_dmm.py
@@ -14,32 +14,15 @@ for i in range(3):
 
 df_treino = df_treino.drop(df_teste.index)
 
-#5-fold
-fold1 =  df_treino.sample(frac=.2)
-df_treino = df_treino.drop(fold1.index)
-
-fold2 =  df_treino.sample(frac=.2)
-df_treino = df_treino.drop(fold2.index)
-
-fold3 =  df_treino.sample(frac=.2)
-df_treino = df_treino.drop(fold3.index)
-
-fold4 =  df_treino.sample(frac=.2)
-df_treino = df_treino.drop(fold4.index)
-
-fold5 =  df_treino
-df_treino = df_treino.drop(fold5.index)
+folds = kfold(df_treino, 5)
 
 #Vetores prototipos de cada classe
 prototipos = df_treino.groupby(df_treino['class']).mean()
 print("prototipos")
 print(prototipos)
 
-
-
-
 #calcula distancia quadrada para cada prototipos
-
+n
 #escolhe a de menor distancia
 result = dmm(prototipos, df_teste)
 
@@ -50,6 +33,20 @@ def dmm(prototipos, dataTeste):
         for class_mean in prototipos:
             distance = mahalanobis_distance(class_mean, novo_padrao)
             
+import random
+def kfold(dataset, k, seed = 42):
+    
+    size = len(dataset)
+    subset_size = round(size / k)
+    random.Random(seed).shuffle(dataset)
+    subsets = [dataset[x:x+subset_size] for x in range(0, len(dataset), subset_size)]
+    kfolds = []
+    for i in range(k):
+        test = subsets[i]
+        train = []
+        for subset in subsets:
+            if subset != test:
+                train.append(subset)
+        kfolds.append((train,test))
         
-
-
+    return kfolds
